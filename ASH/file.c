96a006adff572d2bc9b15ad047b6cc40c78cb609
ashwini ashwini <ashwini.r1@s.amity.edu>
2025-02-19 14:16:21 +0530

    print Course

--- ASH/file.c
+++ ASH/file.c
@@ -2,5 +2,6 @@
 int main() {
 	printf("Hello World!\n");
 	printf("ASHWINI HR\n");
-	printf("AMITY UNIVERSITY\n")
+	printf("AMITY UNIVERSITY\n");
+	printf("B.tech Computer Science Engineering\n");	
 }
