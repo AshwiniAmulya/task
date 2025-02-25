6e5aed1e09eb79d8b2278399a1de98b71bc57295
ASHWINI HR <ashwini.r1@s.amity.edu>
2025-02-25 15:30:18 +0530

    courses offered in college

---
+++ ASH/tag.c
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>My College</title>
+</head>
+<body bg colour ="red">
+<center><h1>my College</h1></center>
+<h3><u>Address</u></h3>
+<h5>Devanhalli<br>Bangalore</br>Karnataka</h5>
+<big><u><i>Courses Offered</i></u></big>
+<small>B.tech<br>MBA<br>BCA,BBA</small>
+</body>
+</html>
