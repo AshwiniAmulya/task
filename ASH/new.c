11833263181dc7fad4a2d03a3ec2d2927cdc77cd
ASHWINI HR <ashwini.r1@s.amity.edu>
2025-02-28 20:20:58 +0530

    Days in a Weak

---
+++ ASH/new.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+	int day;
+	printf("Enter a number(1-7) for the day of the weak:");
+	scanf("%d",&day);
+	switch(day) {
+case1:
+printf("SUNDAY/n");
+break;
+case2:
+printf("MONDAY/n");
+break;
