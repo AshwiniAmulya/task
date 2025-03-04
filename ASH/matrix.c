b6460f92abe0195379fea5b885e0da2b1795bbf5
ASHWINI HR <ashwini.r1@s.amity.edu>
2025-03-04 19:43:14 +0530

    Matrix

--- ASH/matrix.c
+++ ASH/matrix.c
@@ -21,3 +21,12 @@ for(int i=0;i<rows;i++) {
 		sum[i][j]=A[i][j]+B[i][j];
 	}
 }
+printf("\nSum of matrices:\n");
+for(int i=0;i<rows;i++){
+	for(int j=0;j<cols;j++){
+		printf("%d",sum[i][j]);
+	}
+	printf("\n");
+}
+}
+
